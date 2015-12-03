a8f1b8cc317737c12d8fea7c1d6c8df2d7a17f3f
James M. Allen <james.m.allen@gmail.com>
2015-12-03 10:53:01 -0500

    day 3 complete

--- day3.py
+++ day3.py
@@ -1,13 +1,14 @@
 
 
-def santa_run(directions):
+def santa_run(directions, santas=1):
     origin = (0, 0)
-    position = origin
+    positions = [origin] * santas
+    current_mover = 0
     houses = set()
     houses.add(origin)
 
     for d in directions:
-        x, y = position
+        x, y = positions[current_mover]
         if d == '^':
             y += 1
         elif d == '>':
@@ -16,15 +17,19 @@ def santa_run(directions):
             y -= 1
         elif d == '<':
             x -= 1
-        position = x, y
-        houses.add(position)
+        positions[current_mover] = x, y
+        houses.add(positions[current_mover])
+        current_mover += 1
+        current_mover %= santas
 
     return len(houses)
 
 
 def main():
     with open('day3input.txt') as f:
-        print(santa_run(f.read()))
+        directions = f.read()
+        print(santa_run(directions))
+        print(santa_run(directions, santas=2))
 
 if __name__ == '__main__':
     main()
--- test_day3.py
+++ test_day3.py
@@ -12,3 +12,16 @@ def test_santa_run():
 
 def santa_run_checker(directions, houses):
     assert santa_run(directions) == houses
+
+
+def test_robo_run():
+    for directions, houses in [
+        ('^v', 3),
+        ('^>v<', 3),
+        ('^v^v^v^v^v', 11),
+    ]:
+        yield robo_run_checker, directions, houses
+
+
+def robo_run_checker(directions, houses):
+    assert santa_run(directions, santas=2) == houses
