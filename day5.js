911b1357f7b4d775f889dc306ed11ba961c51505
James M. Allen <james.m.allen@gmail.com>
2015-12-14 20:11:56 -0500

    day 5 part b

--- day5.js
+++ day5.js
@@ -8,6 +8,7 @@
   // }
 
   exports.isNice = isNice;
+  exports.isNice2 = isNice2;
   exports.countSubstring = countSubstring;
 
   function countSubstring(haystack, needle) {
@@ -28,6 +29,19 @@
     }
   }
 
+  function isNice2(s) {
+    // It contains a pair of any two letters that appears at least twice in the string without overlapping, like xyxy (xy) or aabcdefgaa (aa), but not like aaa (aa, but it overlaps).
+    // It contains at least one letter which repeats with exactly one letter between them, like xyx, abcdefeghi (efe), or even aaa.
+
+    if (s.match(/([a-zA-Z]{2}).*\1/) &&
+        s.match(/([a-zA-Z]).\1/)) {
+      return true;
+    } else {
+      return false;
+    }
+
+  }
+
   var main = function() {
     // main code
     var rl = require('readline').createInterface({
@@ -35,13 +49,18 @@
     });
 
     var niceCount = 0;
+    var nice2Count = 0;
     rl.on('line', function(line) {
       if (isNice(line)) {
         niceCount++;
       }
+      if (isNice2(line)) {
+        nice2Count++;
+      }
     });
     rl.on('close', function() {
       console.log(niceCount);
+      console.log(nice2Count);
     });
 
   }
--- test/day5.js
+++ test/day5.js
@@ -11,7 +11,7 @@
 var assert = require('assert'),
     expect = require('chai').expect,
     main = require('../day5.js');
-describe('day5', function() {
+describe('day5 part a', function() {
   it('should find nice strings', function() {
     expect(main.isNice('ugknbfddgicrmopn')).to.be.ok;
     expect(main.isNice('aaa')).to.be.ok;
@@ -32,5 +32,23 @@ describe('helper functions', function() {
   it('should count occurrences of regexs in a substring', function() {
     expect(main.countSubstring('the quick brown fox', '[aeiou]')).to.equal(5);
     expect(main.countSubstring('zyzzygy', '[aeiou]')).to.equal(0);
-  })
+  });
+});
+describe('day5 part b', function() {
+  it('should find nice strings', function() {
+    [
+      'qjhvhtzxzqqjkmpb',
+      'xxyxx'
+    ].forEach(function(item) {
+      expect(main.isNice2(item)).to.be.ok;
+    });
+  });
+  it('should find naughty strings', function() {
+    [
+      'uurcxstgmygtbstg',
+      'ieodomkazucvgmuy'
+    ].forEach(function(item) {
+      expect(main.isNice2(item)).to.not.be.ok;
+    });
+  });
 })
